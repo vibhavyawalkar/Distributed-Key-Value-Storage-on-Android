cb83ab9f53f39deffcaa0500581128cd544a1223
Vibhav Yawalkar <vibhavyawalkar@gmail.com>
2020-04-29 02:31:29 -0400

    Ring formation successful

--- SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -31,10 +31,22 @@ import java.io.InputStreamReader;
 import java.lang.*;
 import java.util.HashMap;
 
+class nodeExists {
+    String portStr;
+    int joined;
+
+    public nodeExists(String port, int joinStatus) {
+        this.portStr = port;
+        this.joined = joinStatus;
+    }
+}
+
 public class SimpleDhtProvider extends ContentProvider {
     static final String TAG = SimpleDhtProvider.class.getSimpleName();
     static final String [] portList = {"11108", "11112", "11116", "11120", "11124"};
     static final int SERVER_PORT = 10000;
+    ArrayList<nodeExists> n = new ArrayList<nodeExists>();
+    HashMap<String, String> hashes = new HashMap<String, String>();
     HashMap<String, String> portHash = new HashMap<String, String>();
     static ArrayList<String> insertedKeyList = new ArrayList<String>();
     Uri mUri = buildUri("content", "edu.buffalo.cse.cse486586.simpledht.provider");
@@ -64,13 +76,44 @@ public class SimpleDhtProvider extends ContentProvider {
         return null;
     }
 
+    protected void populateHash() {
+        try {
+            hashes.put("11108", genHash("5554"));
+            hashes.put("11112", genHash("5556"));
+            hashes.put("11116", genHash("5558"));
+            hashes.put("11120", genHash("5560"));
+            hashes.put("11124", genHash("5562"));
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "No such algorithm exception");
+        }
+    }
+
+    protected void populateArrayList() {
+        n.add(new nodeExists("11124", 0));
+        n.add(new nodeExists("11112", 0));
+        n.add(new nodeExists("11108", 0));
+        n.add(new nodeExists("11116", 0));
+        n.add(new nodeExists("11120", 0));
+    }
+
+    protected void setJoined(String port) {
+        for(int i = 0; i < n.size(); i++) {
+            if(port.compareTo(n.get(i).portStr) == 0) {
+                n.get(i).joined = 1;
+            }
+        }
+    }
+
     @Override
     public boolean onCreate() {
         // TODO Auto-generated method stub
+        populateHash();
+        populateArrayList();
         TelephonyManager tel = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
         String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() -4);
         final String myPort = String.valueOf(Integer.parseInt(portStr)*2);
         nodePort = myPort;
+        setJoined(myPort);
         try {
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
             new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
@@ -80,6 +123,7 @@ public class SimpleDhtProvider extends ContentProvider {
         }
         try {
             node = genHash(portStr);
+            Log.e(TAG, "My Hash:" + node);
         } catch(NoSuchAlgorithmException e) {
             Log.e(TAG, "NO such algorithm exception");
         }
@@ -89,36 +133,52 @@ public class SimpleDhtProvider extends ContentProvider {
             String msg = "JOIN" + "-" + myPort;
             new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg);
         } else {
-            Log.e(TAG, "My hash: " + node);
+            Log.e(TAG, "Didn't send the join request: ");
         }
 
         return true;
     }
 
+    protected void sendJoinRequest(String msg) {
+        try {
+            Socket s = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}), Integer.parseInt("11108"));
+            String msgToSend = msg + "\n";
+            // Send Joining request
+            Log.e("sendJoinRequest", "Send joining request");
+            OutputStream out = s.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(out);
+            dos.writeBytes(msgToSend);
+            dos.flush();
+
+            Log.e("sendJoinRequest", "Receive response for joining request");
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String receive = br.readLine();
+
+            String [] msgtokens = receive.split("-");
+
+            if(msgtokens[0].equalsIgnoreCase("PREDSUCC")) {
+                // Update the successor and predecessor
+                predNode = hashes.get(msgtokens[1]);
+                succNode = hashes.get(msgtokens[2]);
+                predPort = msgtokens[1];
+                succPort = msgtokens[2];
+                Log.e(TAG, "PredHash:" + predNode + " " + "SuccHash:" + succNode);
+            } else {
+                Log.e(TAG,"Invalid response to the joining request");
+            }
+        } catch(UnknownHostException e) {
+            Log.e("sendJoinRequest", "UnknownHostException");
+        } catch(IOException e) {
+            Log.e("sendJoinRequest", "IOException");
+        } catch(Exception e) {
+            Log.e("sendJoinRequest", "Generic Exception" + e);
+        }
+    }
+
     private class ClientTask extends AsyncTask<String, Void, Void> {
         @Override
         protected Void doInBackground(String... msgs) {
-            try {
-                Socket s = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}), Integer.parseInt("11108"));
-                String msgToSend = msgs[0].trim() + "\n";
-                // Send Joining request
-                OutputStream out = s.getOutputStream();
-                DataOutputStream dos = new DataOutputStream(out);
-                dos.writeBytes(msgToSend);
-                dos.flush();
-
-                // Receive response
-                BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                String receive = br.readLine();
-                //Update the successor and predecessor
-
-            } catch(UnknownHostException e) {
-                Log.e(TAG, "ClientSide UnknownHostException");
-            } catch(IOException e) {
-                Log.e(TAG, "ClientSide IOException");
-            } catch(Exception e) {
-                Log.e(TAG, "ClientSide Generic Exception");
-            }
+            sendJoinRequest(msgs[0].trim());
             return null;
         }
     }
@@ -158,35 +218,41 @@ public class SimpleDhtProvider extends ContentProvider {
 
     protected void handleRequest(String message, PrintStream ps) {
         Log.e(TAG, "Entering handleRequest");
-        String [] msgtokens = message.split("-", 2);
+        String [] msgtokens = message.split("-");
 
         Log.e(TAG, "Request from client " + msgtokens[0]);
         if(msgtokens[0].equalsIgnoreCase("JOIN")){
             try {
                 String nodePort = msgtokens[1];
-                String nodeHash = genHash(String.valueOf(Integer.parseInt(nodePort)/2));
-                addToRing(nodePort, nodeHash);
-            } catch (NoSuchAlgorithmException e) {
-                Log.e(TAG, "ServerSide no such algorithm exception");
-            } catch (Exception e) {
-                Log.e(TAG, "ServerSide generic exception");
+                //String nodeHash = genHash(String.valueOf(Integer.parseInt(nodePort)/2));
+                //addToRing(nodePort, nodeHash);
+                addToRingNew(nodePort, ps);
+            } catch(Exception e) {
+                Log.e(TAG, "Caught execption on the server, add to Ring");
             }
-            String msgToSend = "ack"+ "\n";
-            ps.println(msgToSend);
-
+        } else if(msgtokens[0].equalsIgnoreCase("PREDSUCC")) {
+            predNode = hashes.get(msgtokens[1]);
+            succNode = hashes.get(msgtokens[2]);
+            predPort = msgtokens[1];
+            succPort = msgtokens[2];
             Log.e(TAG, "PredHash:" + predNode +" " + "SuccHash:" + succNode);
         } else if(msgtokens[0].equalsIgnoreCase("PRED")) {
-            predNode = msgtokens[1];
+            predNode = hashes.get(msgtokens[1]);
+            predPort = msgtokens[1];
+            Log.e(TAG, "PredHash:" + predNode +" " + "SuccHash:" + succNode);
         } else if(msgtokens[0].equalsIgnoreCase("SUCC")) {
-            succNode = msgtokens[1];
+            succNode = hashes.get(msgtokens[1]);
+            succPort = msgtokens[1];
+            Log.e(TAG, "PredHash:" + predNode +" " + "SuccHash:" + succNode);
         } else if(msgtokens[0].equalsIgnoreCase("INSERT")) {
             ContentValues values = new ContentValues();
-            values.put(msgtokens[1], msgtokens[2]);
+            values.put("key", msgtokens[1]);
+            values.put("value",msgtokens[2]);
             insert(mUri, values);
         } else if(msgtokens[0].equalsIgnoreCase("QUERY")) {
             try {
-                String filename = genHash("DHT" + msgtokens[1]);
-                if(node.compareTo(filename) > 0 && predNode.compareTo(filename) < 0) {
+                String filename = genHash(msgtokens[1]);
+                if(node.compareTo(filename) >= 0 && predNode.compareTo(filename) < 0) {
                     String [] col = {"key", "value"};
                     String keyval = "";
                     MatrixCursor cur = (MatrixCursor)query(mUri, null, msgtokens[1], null, null);
@@ -237,8 +303,7 @@ public class SimpleDhtProvider extends ContentProvider {
                     String val = cur.getString(cur.getColumnIndex("value"));
                     keyval = keyval + key + "-" + val + "-";
                 } while(cur.moveToNext());
-                cur.close();
-                if(keyval.length() > 0) {
+                cur.close();if(keyval.length() > 0) {
                     keyval = keyval.substring(0, keyval.length()-1); // Removing the last extra "-"
                 }
             }
@@ -249,66 +314,75 @@ public class SimpleDhtProvider extends ContentProvider {
         return;
     }
 
-    protected void addToRing(String port, String hash) {
-        Log.e(TAG, "Entering addToRing " + port + " " +hash);
-        if(node.compareTo(hash) > 0) { // Less than my hash
-            Log.e(TAG, hash + " less than " + node);
-            if(predNode == null) {
-                Log.e(TAG, "Checkpoint 2");
-                predNode = hash;
-                predPort = port;
-                //add to hash port map
-
-            } else if(predNode.compareTo(hash) < 0 ){ // less than my hash but greater than pred
-                //Lies between me and predecessor
-                Log.e(TAG, "Checkpoint 3");
-                String tmpNode = predNode;
-                String tmpPort = predPort;
-                predNode = hash;
-                predPort = port;
-                // add to hash port map
-                String msg = "SUCC" + "-" + hash;
-                sendUpdate(tmpPort, msg); // Update successor
-                // Send message to the joining node to update its successor and predecessor
-                String msg1 = "SUCC" + "-" + node; // myhash
-                sendUpdate(predPort, msg1);
-                String msg2 = "PRED" + "-" + tmpNode;
-                sendUpdate(predPort, msg2);
-            } else { // Forward joining request to the predecessor
-                Log.e(TAG, "Checkpoint 4");
-                String msg = "JOIN" + "-" + port;
-                sendUpdate(predPort, msg);
-            }
-
-        } else if(node.compareTo(hash) < 0) { // Greater than my hash
-            Log.e(TAG, hash + "  than " + node);
-            if(succNode == null) {
-                Log.e(TAG, "Checkpoint 5");
-                succNode = hash;
-                succPort = port;
-                // Add to hash port map
-            } else if(succNode.compareTo(hash) > 0) { // greater than my hash but less than succ
-                // Lies between me and succ
-                Log.e(TAG, "Checkpoint 6");
-                String tmpNode = succNode;
-                String tmpPort = succPort;
-                succNode = hash;
-                succPort = port;
-                // add to hash port map
-                String msg = "PRED" + "-" + hash;
-                sendUpdate(tmpPort, msg); // Update predecessor
-                // Send message to the joining node to update its successor and predecessor
-                String msg1 = "SUCC" + "-" + tmpNode;
-                sendUpdate(succPort, msg1);
-                String msg2 = "PRED" + "-" + node;
-                sendUpdate(succPort, msg2);
-            } else { // Forward joining request to successor
-                String msg = "JOIN" + "-" + port;
-                Log.e(TAG, "Checkpoint 7");
-                sendUpdate(succPort, msg);
+    protected  void addToRingNew(String port, PrintStream ps) {
+        Log.e(TAG, "Entering add to Ring new for port : " + port);
+        int index = -1;
+        for(int i = 0; i < n.size(); i++)
+        {
+            Log.e(TAG, "Port at index:" + i +" " + n.get(i).portStr);
+            if(port.compareTo(n.get(i).portStr) == 0) {
+                index = i;
+                break;
             }
         }
-        Log.e(TAG, "Successfully added to the ring");
+        Log.e(TAG, "Index of the joining node: " + index);
+        n.get(index).joined = 1;
+        // Start traversing backwards from index to find the predecessor
+        int i;
+        if(index == 0)
+            i = n.size()-1;
+        else
+            i = index-1;
+
+        while(n.get(i).joined != 1) {
+            Log.e(TAG, "Entering while loop" + " " + n.get(i).portStr + "-" +n.get(i).joined);
+            if(i == 0) {
+                i = n.size()-1;
+                continue;
+            }
+            i--;
+        }
+        // Here we found the predecessor
+        String predecessor = n.get(i).portStr;
+
+        // Start traversing forward from index to find the successor
+        int j;
+        if(index == n.size()-1)
+            j = 0;
+        else
+            j = index + 1;
+        while(n.get(j).joined != 1) {
+            Log.e(TAG, "Entering while loop" + " " + n.get(j).portStr + "-" +n.get(j).joined);
+            if(j == n.size()-1) {
+                j = 0;
+                continue;
+            }
+            j++;
+        }
+
+        // Here we found the successor
+        String successor = n.get(j).portStr;
+        String msg = "PREDSUCC" + "-" + predecessor + "-" + successor;
+        Log.e(TAG, "Update successor: " + successor + " " + "predecessor:" + predecessor + " for node:" + port);
+        ps.println(msg); // update predecessor and successor
+
+        if(predecessor.compareTo("11108") == 0) {
+            succPort = port;
+            succNode = hashes.get(port);
+            Log.e(TAG, "Update successor for 11108 to : " + succPort);
+        } else {
+            String msg1 = "SUCC" + "-" + port;
+            sendUpdate(predecessor, msg1);
+        }
+
+        if(successor.compareTo("11108") == 0) {
+            predPort = port;
+            predNode = hashes.get(port);
+            Log.e(TAG, "Update predecessor for 11108 to : " + predPort);
+        } else {
+            String msg2 = "PRED" + "-" + port;
+            sendUpdate(successor, msg2);
+        }
     }
 
     protected MatrixCursor starQuery(MatrixCursor cur) {
@@ -374,7 +448,7 @@ public class SimpleDhtProvider extends ContentProvider {
     }
 
     protected void sendUpdate(String port, String msg) {
-        Log.e(TAG,"Entering sendUpdate");
+        Log.e(TAG,"Send Update message " + msg + " to port " + port);
         try {
             Socket s = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}), Integer.parseInt(port));
             String msgToSend = msg + "\n";
@@ -384,16 +458,16 @@ public class SimpleDhtProvider extends ContentProvider {
             dos.writeBytes(msgToSend);
             dos.flush();
 
-            // Receive response
+            /*// Receive response
             BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
             String receive = br.readLine();
-            //Update the successor and predecessor
+            //Update the successor and predecessor */
         } catch(UnknownHostException e) {
-            Log.e(TAG, "ClientSide UnknownHostException");
+            Log.e("sendUodate", "UnknownHostException");
         } catch(IOException e) {
-            Log.e(TAG, "ClientSide IOException");
+            Log.e("sendUpdate", "IOException");
         } catch(Exception e) {
-            Log.e(TAG, "ClientSide Generic Exception");
+            Log.e("sendUpdate", "Generic Exception");
         }
         return;
     }
@@ -401,9 +475,11 @@ public class SimpleDhtProvider extends ContentProvider {
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         // TODO Auto-generated method stub
+        Log.e("insert", "Entering insert for key: " + values.get("key").toString()
+                + " value:" + values.get("value").toString());
         try {
-            String filename = genHash("DHT" + values.get("key").toString());
-            if(node.compareTo(filename) > 0 && predNode.compareTo(filename) < 0) {
+            String filename = genHash(values.get("key").toString());
+            if((succNode == null && predNode == null) || node.compareTo(filename) >= 0 && predNode.compareTo(filename) < 0) {
                 // less than me and greater than my pred, then it lies between me and my pred
                 // so, it belongs to me
 
@@ -438,7 +514,7 @@ public class SimpleDhtProvider extends ContentProvider {
             if (selection.compareTo("@") == 0 || selection.compareTo("*") == 0) { // all local
                 Log.e(TAG, "Query is @");
                 for(int i = 0; i < insertedKeyList.size(); i++) {
-                    String filename = genHash( "DHT" + insertedKeyList.get(i));
+                    String filename = genHash(insertedKeyList.get(i));
                     FileInputStream inputStream;
 
                     inputStream = getContext().openFileInput(filename);
@@ -456,8 +532,10 @@ public class SimpleDhtProvider extends ContentProvider {
                     return starQuery(cursor);
                 }
             } else {
-                String filename = genHash("DHT" + selection);
-                if(node.compareTo(filename) > 0 && predNode.compareTo(filename) < 0) { // Less than my hash
+                String filename = genHash(selection);
+                if((succNode == null && predNode == null) ||
+                        (node.compareTo(filename) >= 0 && predNode.compareTo(filename) < 0)) {
+                    // Less than my hash
                     //and greater than my predecessor hash It belongs to me
                     FileInputStream inputStream;
 
